ea44376c5c3c456dc6a076e5695a3cb76947ff2c
Frannovara <you@example.com>
2020-12-02 02:43:47 -0300

    se completa archivo app.js

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const app = express();
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/views/index.html')
 })
+
+app.get('/contacto', function (req, res){
+    res.sendFile(__dirname + '/views/contacto.html')
+})
+
+app.get('*', function (req, res){
+    res.sendFile(__dirname + '/public/' + req.url)
+})
+
+app.listen(3000, function() {
+    console.log('Para ver el sitio ingresa a http://localhost:3000')
+})
